9080fcd68c171c62b13c68d15d3b8e15c48ec380
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-08-30 10:13:51 -0300

    criado o arquivo para verificar a lista de comprados

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { adicionarItem } from "./js/adicionarItem.js";
+import { adicionarItem } from "./adicionarItem.js";
 
 const botaoSalvarItem = document.getElementById("adicionar-item");
 botaoSalvarItem.addEventListener("click", adicionarItem);
